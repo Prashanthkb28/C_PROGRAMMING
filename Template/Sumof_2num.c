fd9edad940b0bba7c933fa4f18d015e44e7059f9
prashanthkb <126637555+Prashanthkb28@users.noreply.github.com>
2023-10-20 16:50:12 +0530

    Delete Template directory

--- Template/Sumof_2num.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-int main()
-{
-  int num1 num2;
-  printf("enter two numbers:");
-  scanf("%d %d",&num1,&num2);
-  printf("sum of two numbers %d %d is %d ",num1,num2,(num1+num2));
-  return 0;
-}
